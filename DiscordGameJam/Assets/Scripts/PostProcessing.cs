0ecd3df38d4d78bb1ee2689422a16a1600ec68bb
Gnarly Narwhal <gnarlynarwhal8@gmail.com>
2019-03-02 00:05:45 -0800

    Added post processing effect

---
+++ DiscordGameJam/Assets/Scripts/PostProcessing.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PostProcessing : MonoBehaviour {
+
+	[SerializeField]
+	Material material;
+
+	private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+		Graphics.Blit(source, destination, material);
+	}
+}
