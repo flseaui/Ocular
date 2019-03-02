3aadc3b65450af25521059f62fc2211e819b93ef
rewind <sploing1@gmail.com>
2019-03-01 21:09:01 -0800

    Added goal and some test waypoints

---
+++ DiscordGameJam/Assets/Scripts/GoalSensor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GoalSensor : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Goal"))
+        {
+            Debug.Log("GOAL!");
+        }
+    }
+}
