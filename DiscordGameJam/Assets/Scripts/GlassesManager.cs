7409fdd5b031a71f9ff3b201b002a04a63c79bad
rewind <sploing1@gmail.com>
2019-03-02 01:52:38 -0800

    Added GlassesManager, VisibilityController, and basic glasses switching ui

---
+++ DiscordGameJam/Assets/Scripts/GlassesManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class GlassesManager : Singleton<GlassesManager>
+{
+    public enum Glasses
+    {
+        Red,
+        Green,
+        Blue
+    }
+
+    public Action OnGlassesSwapped;
+
+    [SerializeField]
+    private TextMeshProUGUI _currentGlassesText;
+    
+    private Glasses _currentGlasses;
+    public Glasses CurrentGlasses
+    {
+        get => _currentGlasses;
+        set
+        {
+            _currentGlasses = value;
+            OnGlassesSwapped?.Invoke();
+        }
+    }
+
+    public void SwapGlasses()
+    {
+        if (CurrentGlasses == Glasses.Red)
+            CurrentGlasses = Glasses.Green;
+        else if (CurrentGlasses == Glasses.Green)
+            CurrentGlasses = Glasses.Blue;
+        else
+            CurrentGlasses = Glasses.Red;
+
+        _currentGlassesText.text = $"Current Glasses: {CurrentGlasses}";
+    }
+    
+}
---
+++ DiscordGameJam/Assets/Scripts/VisibilityController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisibilityController : MonoBehaviour
+{
+    public enum GlassesColor
+    {
+        
+        White,
+        Red,
+        Yellow,
+        Blue,
+        Cyan,
+        Green,
+        Magenta
+    }
+
+    public GlassesColor Color;
+
+    [SerializeField] private GameObject _floor;
+    
+    void Start()
+    {
+        GlassesManager.Instance.OnGlassesSwapped += () =>
+        {
+            if (Color == GlassesColor.White) return;
+            switch (GlassesManager.Instance.CurrentGlasses)
+            {
+                case GlassesManager.Glasses.Red:
+                    if (Color == GlassesColor.Red || Color == GlassesColor.Yellow || Color == GlassesColor.Magenta)
+                        _floor.SetActive(true);
+                    else
+                        _floor.SetActive(false);
+                    break;
+                case GlassesManager.Glasses.Green:
+                    if (Color == GlassesColor.Green || Color == GlassesColor.Yellow)
+                        _floor.SetActive(true);
+                    else
+                        _floor.SetActive(false);
+                    break;
+                case GlassesManager.Glasses.Blue:
+                    if (Color == GlassesColor.Blue || Color == GlassesColor.Cyan)
+                        _floor.SetActive(true);
+                    else
+                        _floor.SetActive(false);
+                    break;
+            }
+        };
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
