339af28e2a1e9d2449846ccbcb51473bb0c45375
rewind <sploing1@gmail.com>
2019-03-01 20:41:04 -0800

    Finished A* and movement in PathManager

--- DiscordGameJam/Assets/Scripts/PathManager.cs
+++ DiscordGameJam/Assets/Scripts/PathManager.cs
@@ -30,17 +30,63 @@ public class PathManager : MonoBehaviour
             openList.RemoveAt(0);
             var dist = currentNode.Distance;
             closedList.Add(currentNode);
+            if (currentNode == endNode)
+            {
+                break;
+            }
+
+            foreach (var neighbor in currentNode.Neighbors)
+            {
+                if (closedList.Contains(neighbor) || openList.ContainsValue(neighbor))
+                    continue;
+                neighbor.Previous = currentNode;
+                neighbor.Distance = dist + (neighbor.transform.position - currentNode.transform.position).magnitude;
+                var distanceToTarget = (neighbor.transform.position - endNode.transform.position).magnitude;
+                openList.Add(neighbor.Distance + distanceToTarget, neighbor);
+            }
+
+            if (currentNode == endNode)
+            {
+                while (currentNode.Previous != null)
+                {
+                    _currentPath.Push(currentNode.transform.position);
+                    currentNode = currentNode.Previous;
+                }
+                _currentPath.Push(transform.position);
+            }
         }
     }
 
     public void Stop()
     {
-        
+        _currentPath = null;
+        _moveTimeTotal = 0;
+        _moveTimeCurrent = 0;
     }
 
     private void Update()
     {
-        
+        if (_currentPath != null && _currentPath.Count > 0)
+        {
+            if (_moveTimeCurrent < _moveTimeTotal)
+            {
+                _moveTimeCurrent += Time.deltaTime;
+                if (_moveTimeCurrent > _moveTimeTotal)
+                    _moveTimeCurrent = _moveTimeTotal;
+                transform.position = Vector3.Lerp(_currentWaypointPosition, _currentPath.Peek(), _moveTimeCurrent / _moveTimeTotal);
+            }
+            else
+            {
+                _currentWaypointPosition = _currentPath.Pop();
+                if (_currentPath.Count == 0)
+                    Stop();
+                else
+                {
+                    _moveTimeCurrent = 0;
+                    _moveTimeTotal = (_currentWaypointPosition - _currentPath.Peek()).magnitude / WalkSpeed;
+                }
+            }
+        }
     }
 
     private Waypoint FindClosestWaypoint(Vector3 target)
