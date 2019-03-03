64532088a1a56afa633c0eade7a4d7ea067b0cc1
Vincent Migliore <vincenthepic@gmail.com>
2019-03-02 18:21:06 -0800

    Scratch-Kin

---
+++ DiscordGameJam/Assets/Scripts/Button.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    enum buttonType
+    {
+        Toggle,
+        Hold
+    }
+
+    public bool state;
+
+    private buttonType type;
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.transform.tag == "Player" || col.transform.tag == "Box")
+        {
+            if (type == buttonType.Hold)
+                state = true;
+            else
+                state = !state;
+        }
+    }
+
+    private void OnCollisionExit(Collision col)
+    {
+        if (col.transform.tag == "Player" || col.transform.tag == "Box")
+        {
+            if (type == buttonType.Hold)
+                state = false;
+        }
+    }
+}
