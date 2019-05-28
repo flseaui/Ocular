b03fa300f5e4f28c394fdf86ede604708e0dfa18
Rewind <sploing1@gmail.com>
2019-05-27 17:11:09 -0700

    Added camera rotation with Left and Right

---
+++ Assets/_Scripts/Misc/CameraController.cs
@@ -0,0 +1,29 @@
+using DG.Tweening;
+using UnityEngine;
+
+namespace Misc
+{
+    public class CameraController : MonoBehaviour
+    {
+        private void Start()
+        {
+            DOTween.Init();
+        }
+
+        private bool _moving;
+
+        private void Update()
+        {
+            if (_moving) return;
+
+            var left = Input.GetKey(KeyCode.LeftArrow);
+            var right = Input.GetKey(KeyCode.RightArrow);
+
+            var angle = left ? right ? 0 : -45 : right ? 45 : 0;
+            
+            _moving = true;
+            var rotate = transform.DOLocalRotate(transform.rotation.eulerAngles + new Vector3(0, angle, 0), .2f);
+            rotate.onComplete += () => _moving = false;
+        }
+    }
+}
