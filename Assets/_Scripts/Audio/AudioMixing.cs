625a74396c7290173261e5a3596c32c01aaddfc6
Vincent Migliore <vincenthepic@gmail.com>
2019-08-05 18:12:58 -0700

    New models added

--- Assets/_Scripts/Audio/AudioMixing.cs
+++ Assets/_Scripts/Audio/AudioMixing.cs
@@ -7,10 +7,10 @@ namespace Audio
 {
     public static class AudioMixing
     {
-        private static readonly float WorldOneAVolume = 0.5f;
-        private static readonly float WorldOneBVolume = .5f;
+        private static readonly float WorldOneAVolume = 0.4f;
+        private static readonly float WorldOneBVolume = .4f;
         private static readonly float WorldOneCVolume = 0.5f;
-        private static readonly float WorldOneMainVolume = .6f;
+        private static readonly float WorldOneMainVolume = .7f;
 
         public static readonly float FadeInTime = 1.5f;
         public static readonly float FadeOutTime = 3f;
