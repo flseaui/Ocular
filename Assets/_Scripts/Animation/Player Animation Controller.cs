82b762db74a019f449869bd55fc1f1643d46a0f6
Vincent Migliore <vincenthepic@gmail.com>
2019-08-06 17:23:25 -0700

    Start of animations

---
+++ Assets/_Scripts/Animation/Player Animation Controller.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Animation
+{
+    internal abstract class Frame
+    {
+        private MeshFilter _mesh;
+        private Material _material;
+    }
+
+    struct Animation
+    {
+        private List<Frame> _frames;
+        private List<int> _timing;
+        private bool _looping;
+        private int _currentFrame;
+
+        Animation(List<Frame> frameList)
+        {
+            _frames = frameList;
+            _currentFrame = 0;
+            _looping = false;
+        }
+
+        Frame Play()
+        {
+            if (_currentFrame >= _frames.Count)
+            {
+                if (!_looping)
+                {
+                    Stop();
+                    return null;
+                }
+                _currentFrame = 0;
+            }
+
+            Frame nextFrame = _frames[_currentFrame];
+            _currentFrame++;
+            return nextFrame;
+        }
+
+        void Loop()
+        {
+            _looping = true;            
+        }
+
+        void Stop()
+        {
+            _currentFrame = 0;
+            _looping = false;
+        }
+
+    }
+
+    public class PlayerAnimationController : MonoBehaviour
+    {
+        
+        
+    }
+}
