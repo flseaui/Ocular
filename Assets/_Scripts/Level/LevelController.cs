c4f08b613c3d8275d2c6d437fc07783c8688b76c
Vincent Migliore <vincenthepic@gmail.com>
2019-08-13 00:15:20 -0700

    Level controller changed to support multiple worlds

--- Assets/_Scripts/Level/LevelController.cs
+++ Assets/_Scripts/Level/LevelController.cs
@@ -1,39 +1,103 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Sirenix.OdinInspector;
 using Sirenix.Utilities;
 using UnityEngine;
 
 namespace Level {
+    
+    public static class Extensions
+    {
+        public static Tuple<int, int> CoordinatesOf<T>(this T[,] matrix, T value)
+        {
+            int w = matrix.GetLength(0);
+            int h = matrix.GetLength(1);
+
+            for (int x = 0; x < w; ++x)
+            {
+                for (int y = 0; y < h; ++y)
+                {
+                    if (matrix[x, y].Equals(value))
+                        return Tuple.Create(x, y);
+                }
+            }
+
+            return Tuple.Create(-1, -1);
+        }
+        
+        public static Tuple<int, int> GetNext<T>(this T[,] matrix, Tuple<int, int> value)
+        {
+            int w = matrix.GetLength(0);
+            int h = matrix.GetLength(1);
+
+            for (int x = value.Item1; x < w; ++x)
+            {
+                for (int y = value.Item2; y < h; ++y)
+                {
+                    if (!matrix[x, y].Equals(null))
+                        return Tuple.Create(x, y);
+                }
+            }
+
+            return Tuple.Create(-1, -1);
+        }
+    }
+
     public class LevelController : MonoBehaviour
     {
-        [SerializeField] private List<GameObject> _levels;
+        [SerializeField] private List<GameObject> _worldOneLevels;
+        [SerializeField] private List<GameObject> _worldTwoLevels;
+
+        private List<List<GameObject>> _worlds;
+        
+        private GameObject[,] _levels = new GameObject[2,6];
 
         private GameObject _loadedLevel;
-        private int _loadedLevelNumber;
+        private Tuple<int, int> _loadedLevelNumber;
 
         [HideInInspector]
         public LevelInfo CurrentLevelInfo;
 
-        [ValueDropdown("_levels")] public GameObject StartingLevel;
-
-        private int LevelCount => _levels.Count;
+        [ValueDropdown("_worldOneLevels")] public GameObject StartingLevel;
 
         public static Action OnLevelLoaded;
 
-        private int _levelToLoad;
-        
+        private Tuple<int, int> _levelToLoad;
+
+        private void Awake()
+        {      
+            _worlds = new List<List<GameObject>>
+            {
+                _worldOneLevels,
+                _worldTwoLevels
+            };
+            
+            var worldIndex = 0;
+            foreach (var world in _worlds)
+            {
+                var levelIndex = 0;
+                foreach (var level in world)
+                {
+                    _levels[worldIndex, levelIndex] = level;
+                    levelIndex++;
+                }
+
+                worldIndex++;
+            }
+        }
+
         public IEnumerator LoadNextLevel()
         {
-            _levelToLoad = _loadedLevelNumber + 1;
+            _levelToLoad = _levels.GetNext(_loadedLevelNumber);
             StartCoroutine(nameof(LoadLevel));
             yield return new WaitForSeconds(1);
         }
 
         public IEnumerator LoadFirstLevel()
         {
-            _levelToLoad = _levels.IndexOf(StartingLevel);
+            _levelToLoad = _levels.CoordinatesOf(StartingLevel);
             StartCoroutine(nameof(LoadLevel));
             
             yield return new WaitForSeconds(1);
@@ -57,7 +121,7 @@ namespace Level {
             
             yield return new WaitForEndOfFrame();
             
-            _loadedLevel = Instantiate(_levels[_levelToLoad]);
+            _loadedLevel = Instantiate(_levels[_levelToLoad.Item1, _levelToLoad.Item2]);
             _loadedLevel.gameObject.SetActive(true);
             _loadedLevel.GetComponent<MapController>().FindNeighbors();
             CurrentLevelInfo = _loadedLevel.GetComponent<LevelInfo>();
@@ -70,5 +134,6 @@ namespace Level {
             Destroy(_loadedLevel);
             CurrentLevelInfo = null;
         }
+        
     }
 }
