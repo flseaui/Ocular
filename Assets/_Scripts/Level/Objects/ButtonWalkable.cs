47a92d81318784515d24e5f1dfe8c94a53d62caf
Rewind <sploing1@gmail.com>
2019-10-08 17:28:59 -0700

    Button indicator lines

--- Assets/_Scripts/Level/Objects/ButtonWalkable.cs
+++ Assets/_Scripts/Level/Objects/ButtonWalkable.cs
@@ -6,7 +6,7 @@ using Misc;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
-namespace Level.Objects 
+namespace Level.Objects
 {
     /// <summary>
     /// A walkable object housing a button, can target multiple blocks and change their color on press.
@@ -19,11 +19,13 @@ namespace Level.Objects
 
         private Vector3 _initialPosition;
 
+        private List<GameObject> _lines;
+
         /// <summary>
         /// The blocks that will be modified when the button is pressed.
         /// </summary>
         public List<Colorable> TargetBlocks;
-        
+
         /// <summary>
         /// The state of the <c>Button</c> (true - pressed, false - unpressed).
         /// </summary>
@@ -31,6 +33,11 @@ namespace Level.Objects
         {
             get => _buttonModel.State;
             set => _buttonModel.State = value;
+        }
+
+        protected override void OnAwake()
+        {
+            _lines = new List<GameObject>();
         }
 
         private void Start()
@@ -42,7 +49,7 @@ namespace Level.Objects
 
             // register with all target blocks
             TargetBlocks.ForEach(t => t.RegisterController(this));
-           
+
 
             _buttonModel.OnStateChanged += () =>
             {
@@ -67,6 +74,45 @@ namespace Level.Objects
                     });
                 }
             };
+        }
+
+        private void Update()
+        {
+            if (Enabled)
+            {
+                if (Input.GetKeyDown(KeyCode.LeftShift))
+                {
+                    TargetBlocks.ForEach(t =>
+                    {
+                        var y = _buttonModel.GetComponent<MeshFilter>().sharedMesh.bounds.max.y;
+                        if (t.Outlined)
+                            DrawLine(new Vector3(_buttonModel.transform.position.x, y, _buttonModel.transform.position.z), t.transform.position,
+                                Colorable.StateToColor(t.State == Colorable.BlockState.Visible ? Color : t.OcularState));
+                    });
+                }
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                foreach (var line in _lines)
+                {
+                    Destroy(line.gameObject);
+                }
+            }
+        }
+
+        void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.01f)
+        {
+            var myLine = new GameObject();
+            myLine.transform.position = start;
+            myLine.AddComponent<LineRenderer>();
+            var lr = myLine.GetComponent<LineRenderer>();
+            lr.material = new Material(Shader.Find("Legacy Shaders/Particles/Alpha Blended Premultiply"));
+            lr.SetColors(color, color);
+            lr.SetWidth(0.05f, 0.05f);
+            lr.SetPosition(0, start);
+            lr.SetPosition(1, end);
+            _lines.Add(myLine);
         }
     }
 }
