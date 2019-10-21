78c791959bd385916501dbd7ab297bebe532701a
Claire Meyer <claire.meyer@brokenrecord.studio>
2019-10-21 14:36:44 -0700

    Removed IdleNew and anim zips

--- Assets/_Scripts/Misc/Tools/VoxelAnimationCreator.cs
+++ Assets/_Scripts/Misc/Tools/VoxelAnimationCreator.cs
@@ -1,5 +1,20 @@
 using UnityEngine;
+using UnityEditor;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
 
-public class VoxelAnimationCreator : MonoBehaviour {
-    
+public class VoxelAnimationCreator : OdinEditorWindow {
+    [MenuItem("Tools/Ocular/Voxel Animation Creator")]
+    private static void OpenWindow()
+    {
+        GetWindow<VoxelAnimationCreator>().Show();
+    }
+
+    [SerializeField]
+    private List<GameObject> 
+
+    [Button(ButtonSizes.Large), GUIColor(0, 1, 0)]
+    public void CreateAnimation() {
+
+    }
 }
