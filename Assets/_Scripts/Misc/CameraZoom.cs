a266beede75707bbd733f8297ec041dabdac4f78
Rewind <sploing1@gmail.com>
2019-10-30 12:36:10 -0700

    Camera mouse wheel zoom

---
+++ Assets/_Scripts/Misc/CameraZoom.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Misc
+{
+    public class CameraZoom : MonoBehaviour
+    {
+
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = GetComponent<Camera>();
+        }
+
+        private void Update()
+        {
+            var scrollDelta = Input.mouseScrollDelta.y;
+            scrollDelta = .3f * -scrollDelta;
+            if (scrollDelta > 0)
+            {
+                if (_camera.orthographicSize + scrollDelta < 10)
+                {
+                    _camera.orthographicSize += scrollDelta;
+                }
+                else
+                {
+                    _camera.orthographicSize = 10;
+                }
+            }
+            else
+            {
+                if (_camera.orthographicSize + scrollDelta > 2)
+                {
+                    _camera.orthographicSize += scrollDelta;
+                }
+                else
+                {
+                    _camera.orthographicSize = 2;
+                }
+            }
+        }
+
+    }
+}
