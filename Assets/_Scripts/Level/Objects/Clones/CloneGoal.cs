c95c2b2bef7381d4a38593120be87ef931f595ca
Claire Meyer <signedrewind@gmail.com>
2020-02-18 23:53:02 -0800

    Fixed 31666 and clone goal

--- Assets/_Scripts/Level/Objects/Clones/CloneGoal.cs
+++ Assets/_Scripts/Level/Objects/Clones/CloneGoal.cs
@@ -5,6 +5,8 @@ namespace Level.Objects.Clones
 {
     public class CloneGoal : MonoBehaviour
     {
+        private bool _used;
+        
         private void Awake()
         {
             Goal.GoalConditions += 1;
@@ -12,8 +14,9 @@ namespace Level.Objects.Clones
 
         private void OnTriggerEnter(Collider other)
         {
-            if (other.CompareTag("Clone"))
+            if (other.CompareTag("Clone") && !_used)
             {
+                _used = true;
                 Goal.GoalConditionsMet += 1;
                 other.transform.GetComponent<ClonePathfinder>().AtGoal = true;
                 other.transform.GetComponent<ClonePathfinder>().Navigating = false;
