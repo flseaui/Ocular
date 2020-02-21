e605d71d3509adbff8b27262810a30ca7258c264
Claire Meyer <signedrewind@gmail.com>
2020-02-21 02:17:20 -0800

    Fixed clone goals breaking normal goals

--- Assets/_Scripts/Level/Objects/Goal.cs
+++ Assets/_Scripts/Level/Objects/Goal.cs
@@ -1,4 +1,5 @@
-﻿using Player;
+﻿using System;
+using Player;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -26,7 +27,13 @@ namespace Level.Objects {
             Satisfied = GoalConditionsMet >= GoalConditions;
             _animator.SetBool(SatisfiedProp, Satisfied);
         }
-        
+
+        private void OnDestroy()
+        {
+            GoalConditions = 0;
+            GoalConditionsMet = 0;
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             if (!Satisfied) return;
